e89de1aa6b8822b876825985e17616fa1c72fbf4
Discusser <47938380+Discusser@users.noreply.github.com>
2025-07-13 16:38:54 +0200

    Remove log statements

--- lua/init.lua
+++ lua/init.lua
@@ -5,8 +5,6 @@ local log = require 'log'
 
 -- Automatically import all plugins in lua/plugins/
 local cwdContent = vim.split(vim.fn.glob(vim.fn.stdpath 'config' .. '/lua/plugins/**/*.lua'), '\n', { trimempty = true })
-log:write 'Ok this is cwdContent'
-log:write(vim.inspect(cwdContent))
 for _, s in pairs(cwdContent) do
   s = s:gsub('/', '.')
   local i, _ = s:find 'lua'
